68ba1269144076cf81fe356e39758e562cc1f823
terezasekyrova <tereza.sekyrova@leveris.com>
2019-04-15 15:59:09 +0200

    First commit

--- src/com/leveris/tera/Main.java
+++ src/com/leveris/tera/Main.java
@@ -3,6 +3,6 @@ package com.leveris.tera;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Dneska ti to sekne kotě!");
+	System.out.println("Funguje to?");
     }
 }
